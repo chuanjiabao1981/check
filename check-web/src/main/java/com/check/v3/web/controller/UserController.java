77531ff0c529485cd0aa4e1b0058ac71a9e3afd4
guowei <chuanjiabao1981@gmail.com>
2013-10-23 22:41:54 +0800

    remove no use package import

--- check-web/src/main/java/com/check/v3/web/controller/UserController.java
+++ check-web/src/main/java/com/check/v3/web/controller/UserController.java
@@ -1,18 +1,13 @@
 package com.check.v3.web.controller;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.validation.Valid;
 
-import org.aspectj.bridge.Message;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.check.v3.domain.User;
 import com.check.v3.security.util.SecurityTools;
